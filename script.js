99b93d3a7b28e45e2a0c0fdc36433c8984e9cc5a
egrosscurt <84039948+egrosscurt@users.noreply.github.com>
2021-06-03 11:56:32 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+var Age = 15;
+var isFemale = false;
+var driverStatus = "bob";
+
+if (Age >= 18){
+
+    console.log("Je mag naar binnen");
+
+} else {
+
+    console.log("Sorry, je bent te jong");
+} 
+
+if (isFemale){
+
+    console.log("Welkom bij de Ladies Night");
+
+} else {
+
+    console.log("Sorry, het is Ladies Night vandaag");
+} 
+
+if (driverStatus == "bob"){
+
+    console.log("Goedzo je bent de bob!");
+
+} else {
+
+    console.log("Zal ik een taxi voor je bellen?");
+} 
